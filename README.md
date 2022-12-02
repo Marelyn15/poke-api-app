1213eefecaaad69d70a2105392f43a6666e5fe5a
Marelyn Deschamps <mdeschampsfranco@gmail.com>
2022-12-02 16:32:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# poke-api-app
+ How to consume a API with a basic program
---
+++ main.js
@@ -0,0 +1,77 @@
+
+let container = document.getElementById("mostrarPokemon");
+
+let enviar = document.getElementById("enviar");
+enviar.onclick = getPokemon; 
+
+let borrar  = document.getElementById("delete");
+borrar.onclick = deletContent;
+
+
+function getPokemon(nombre){
+    
+   //container.innerHTML = nombre;
+/*
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
+    .then((response) => response.json())
+    .then((data) =>{showPokemon(data)
+    
+    });
+    */
+
+    
+        nombre = document.getElementById("poke").value;
+
+    
+        switch(nombre){
+            case 'pikachu':
+                fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+                .then((response) => response.json())
+                .then((data) =>{showPokemon(data) });
+                break;
+            case 'ditto': 
+                fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+                .then((response) => response.json())
+                .then((data) =>{showPokemon(data) });
+                break;
+            case 'bulbasaur':
+                fetch('https://pokeapi.co/api/v2/pokemon/1')
+                .then((response) => response.json())
+                .then((data) =>{showPokemon(data)  });
+                break;
+            case 'snivy':
+                fetch('https://pokeapi.co/api/v2/pokemon/snivy')
+                .then((response) => response.json())
+                .then((data) =>{showPokemon(data)  });
+                break;
+            case 'marshtomp':
+                fetch('https://pokeapi.co/api/v2/pokemon/marshtomp')
+                .then((response) => response.json())
+                .then((data) =>{showPokemon(data)  });
+                break;
+            default: 
+                container.innerHTML = 'no data'
+                break;
+        }  
+    }
+
+   function showPokemon(pokemon){
+    let img = document.createElement('img');
+    img.src = pokemon.sprites.front_shiny;
+
+    let name = document.createElement('h3');
+    name.textContent = pokemon.name;
+
+    let num = document.createElement('p');
+    num.textContent = pokemon.order;
+
+    
+    container.appendChild(img);
+    container.appendChild(name);
+    container.appendChild(num);
+   
+   }
+
+   function deletContent(){
+        container.innerHTML = "";
+   }
